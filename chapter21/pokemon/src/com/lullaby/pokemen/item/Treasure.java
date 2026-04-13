6b67e8d0eeba9976477aeaaf64fadf28270ed3e0
Sonnet <Sonnet@Sonnet14.cn>
2026-04-13 16:45:04 +0800

    增加开启宝箱功能

--- chapter21/pokemon/src/com/lullaby/pokemen/item/Treasure.java
+++ chapter21/pokemon/src/com/lullaby/pokemen/item/Treasure.java
@@ -1,5 +1,7 @@
 package com.lullaby.pokemen.item;
 
+import java.util.Random;
+
 /**
  * 宝箱
  */
@@ -7,6 +9,25 @@ public class Treasure extends Item {
 
     public Treasure(String name, int levelNumber) {
         super("宝箱", levelNumber);
+    }
+
+    /**
+     * 开启宝箱能够获得一个物品
+     * @return
+     */
+    public Item open() {
+        Random random = new Random();
+        // 取[0, 10)之间的随机数
+        int num = random.nextInt(10);
+        if (num == 0) { // 获得宠物小精灵
+
+        } else if (num <= 3) {  // 获得装备
+
+        } else {    // 获得药品
+
+        }
+
+        return null;
     }
 
     @Override
