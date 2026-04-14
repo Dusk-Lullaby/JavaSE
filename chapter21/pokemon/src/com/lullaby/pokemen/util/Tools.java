a8bcf383c0bc500c1026b75637f029be74217e6a
Sonnet <Sonnet@Sonnet14.cn>
2026-04-14 16:35:05 +0800

    将获得随机物品方法变为工具类

--- chapter21/pokemon/src/com/lullaby/pokemen/util/Tools.java
+++ chapter21/pokemon/src/com/lullaby/pokemen/util/Tools.java
@@ -1,5 +1,13 @@
 package com.lullaby.pokemen.util;
 
+import com.lullaby.pokemen.item.HP;
+import com.lullaby.pokemen.item.Item;
+import com.lullaby.pokemen.item.equipment.*;
+import com.lullaby.pokemen.item.pokemon.Bikachu;
+import com.lullaby.pokemen.item.pokemon.Bulbasaur;
+import com.lullaby.pokemen.item.pokemon.Charmander;
+import com.lullaby.pokemen.item.pokemon.Jolteon;
+
 import java.util.Random;
 
 /**
@@ -40,5 +48,56 @@ public class Tools {
      */
     public static int getRandomNumber(int max) {
         return getRandomNumber(0, max, 1);
+    }
+
+    /**
+     * 获取随机物品
+     * @param levelNumber 关卡编号
+     * @return 获得的物品
+     */
+    public static Item getRandomItem(int levelNumber) {
+        Random random = new Random();
+        // 取[0, 10)之间的随机数
+        int num = Tools.getRandomNumber(10);
+        if (num == 0) { // 获得宠物小精灵，初级：中级：高级：究极 = 80:15:4:1
+            int rate = Tools.getRandomNumber(100);
+            if (rate == 0) {    // 究极小精灵
+                return new Bikachu();
+            }
+            if (rate <= 4) {    // 高级
+                return new Charmander();
+            }
+            if (rate <= 20) {   // 中级
+                return new Jolteon();
+            }
+            return new Bulbasaur(); // 初级
+        } else if (num <= 3) {  // 获得装备
+            // 武器：项链：戒指：手镯：头盔：铠甲：护腿：鞋子 = 3：5：8：8：19：19：19：19
+            int rate = Tools.getRandomNumber(100);
+            if (rate < 3) { // 武器
+                return new Weapon(levelNumber);
+            }
+            if (rate < 8) { // 项链
+                return new Necklace(levelNumber);
+            }
+            if (rate < 16) {// 戒指
+                return new Ring(levelNumber);
+            }
+            if (rate < 24) {// 手镯
+                return new Bracelet(levelNumber);
+            }
+            if (rate < 43) {// 头盔
+                return new Helmet(levelNumber);
+            }
+            if (rate < 62) {// 铠甲
+                return new Armor(levelNumber);
+            }
+            if (rate < 81) {// 护腿
+                return new Leggings(levelNumber);
+            }
+            else return new Shoe(levelNumber);  // 鞋子
+        } else {    // 获得药品
+            return new HP(levelNumber, 10);
+        }
     }
 }
